9135a8a973118e7c329a9041b58697f42e6b066b
Sanaz-RH <149827517+Sanaz-RH@users.noreply.github.com>
2025-01-30 16:17:22 +0100

    Added functionality to the select

--- app.js
+++ app.js
@@ -1,35 +1,63 @@
 function updateTime() {
   // Zurich
   let zurichElement = document.querySelector("#zurich");
-  let zurichDateElement = zurichElement.querySelector(".date");
-  let zurichTimeElement = zurichElement.querySelector(".time");
-  let zurichTime = moment().tz("Europe/Zurich");
+  if (zurichElement) {
+    let zurichDateElement = zurichElement.querySelector(".date");
+    let zurichTimeElement = zurichElement.querySelector(".time");
+    let zurichTime = moment().tz("Europe/Zurich");
 
-  zurichDateElement.innerHTML = zurichTime.format("MMMM Do YYYY");
-  zurichTimeElement.innerHTML = zurichTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    zurichDateElement.innerHTML = zurichTime.format("MMMM Do YYYY");
+    zurichTimeElement.innerHTML = zurichTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   // Miami
   let miamiElement = document.querySelector("#miami");
-  let miamiDateElement = miamiElement.querySelector(".date");
-  let miamiTimeElement = miamiElement.querySelector(".time");
-  let miamiTime = moment().tz("America/New_York");
+  if (miamiElement) {
+    let miamiDateElement = miamiElement.querySelector(".date");
+    let miamiTimeElement = miamiElement.querySelector(".time");
+    let miamiTime = moment().tz("America/New_York");
 
-  miamiDateElement.innerHTML = miamiTime.format("MMMM Do YYYY");
-  miamiTimeElement.innerHTML = miamiTime.format("h:mm:ss [<small>]A[</small>]");
+    miamiDateElement.innerHTML = miamiTime.format("MMMM Do YYYY");
+    miamiTimeElement.innerHTML = miamiTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   // Tehran
   let tehranElement = document.querySelector("#tehran");
-  let tehranDateElement = tehranElement.querySelector(".date");
-  let tehranTimeElement = tehranElement.querySelector(".time");
-  let tehranTime = moment().tz("Asia/Tehran");
+  if (tehranElement) {
+    let tehranDateElement = tehranElement.querySelector(".date");
+    let tehranTimeElement = tehranElement.querySelector(".time");
+    let tehranTime = moment().tz("Asia/Tehran");
 
-  tehranDateElement.innerHTML = tehranTime.format("MMMM Do YYYY");
-  tehranTimeElement.innerHTML = tehranTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    tehranDateElement.innerHTML = tehranTime.format("MMMM Do YYYY");
+    tehranTimeElement.innerHTML = tehranTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+
+function updateCity(event) {
+  let cityTimezone = event.target.value;
+  let cityName = cityTimezone.split("/")[1].replace("_", " ");
+  let cityTime = moment().tz(cityTimezone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+    <div class="city" >
+        <div>
+        <h2>${cityTimezone}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format(
+          "h:mm:ss"
+        )} <small>${cityTime.format("A")}</small></div>
+    </div>`;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citySelectElement = document.querySelector("#city");
+citySelectElement.addEventListener("change", updateCity);
